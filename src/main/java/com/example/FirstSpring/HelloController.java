f5d38942ce6626fc12c3a90e2289ee74d8ac2b03
“bharath669” <vvbharath2006@gmail.com>
2025-09-19 17:51:18 +0100

    2 dyanamic endpoints

--- src/main/java/com/example/FirstSpring/HelloController.java
+++ src/main/java/com/example/FirstSpring/HelloController.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello/{name}")
-    public HelloResponse hello(@PathVariable String name){
-        return new HelloResponse("Hello world! "+name);
-    }
-    @GetMapping("/hello")
-    public HelloResponse hello(){
-        return new HelloResponse("Hello world!");
-    }
-    @PostMapping("/hello")
-    public HelloResponse helloPost(@RequestBody  String name){
-        return new HelloResponse("hello " +name+ " !");
-    }
+    @GetMapping("/hello/{name}/{show}")
+    public HelloResponse hello(@PathVariable String name,@PathVariable String show){
+        return new HelloResponse("Hello world! "+name,"state "+show);
+    }//while hitting URL in postman the endpoints what i give it will reflected in o/p
+//    @GetMapping("/hello")
+//    public HelloResponse hello(){
+//        return new HelloResponse("Hello world!");
+//    }
+//    @PostMapping("/hello")
+//    public HelloResponse helloPost(@RequestBody  String name){
+//        return new HelloResponse("hello " +name+ " !");
+//    }
 }
--- src/main/java/com/example/FirstSpring/HelloResponse.java
+++ src/main/java/com/example/FirstSpring/HelloResponse.java
@@ -2,13 +2,18 @@ package com.example.FirstSpring;
 
 public class HelloResponse {
     private String message;
+    private String message1;
 
-    public HelloResponse(String message) {
+    public HelloResponse(String message,String message1) {
         this.message = message;
+        this.message1=message1;
     }
 
     public String getMessage() {
         return message;
+    }
+    public String getMessage1() {
+        return message1;
     }
 
 //    public void setMessage(String message) {
